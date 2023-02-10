8c23eb70f3f2333f827690c6d2558b7b1364520a
Ivan <ivankobogdantsev@mail.ru>
2023-02-10 18:49:48 +0300

    deleted python, cpp and comments

--- server/index.js
+++ server/index.js
@@ -10,21 +10,14 @@ app.use(express.json());
 app.post("/compile", (req, res) => {
     //getting the required data from the request
     let code = req.body.code;
-    let language = req.body.language;
-    let input = req.body.input;
 
-    if (language === "python") {
-        language = "py"
-    }
 
     let data = ({
-        "code": code,
-        "language": language,
-        "input": input
+        "cmd": "g++ main.cpp && ./a.out",
+        "src": code,
     });
     let config = {
         method: 'post',
-        //url: 'https://codexweb.netlify.app/.netlify/functions/enforceCode',
         url: 'http://coliru.stacked-crooked.com/compile',
         headers: {
             'Content-Type': 'application/json'
@@ -32,12 +25,6 @@ app.post("/compile", (req, res) => {
         data: data
     };
 
-    //     var http = new XMLHttpRequest();
-    // http.open("POST", "http://coliru.stacked-crooked.com/compile", false);
-    // http.send(JSON.stringify({ "cmd": "g++ main.cpp && ./a.out", "src": "int main() { throw 1; }" }));
-    // alert(http.response);
-
-    //calling the code compilation API
     Axios(config)
         .then((response) => {
             res.send(response.data)
